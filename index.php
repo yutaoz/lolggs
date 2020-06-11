9b44eb09fe5943ca280c857308a2f110b2e3891b
yutaoz <47333239+yutaoz@users.noreply.github.com>
2020-06-11 19:20:12 -0400

    Creation of header

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 <html>
   <head>
     <title> Welcome to LOLGGS </title>
+    <link href="home.css" rel="stylesheet" type="text/css">
   </head>
   <body>
-    <form>
-      <label for="champion" class="champion">Find Chamption</label><br>
-      <input type="text" id="championselect" name="championselect" class="champbox"></input>
-    </form>
+    <div class="parent">
+      <div class="header">
+        <h1>lol, ggs!</h1>
+      </div>
+
+
+      <form>
+        <label for="champion" class="champion">Find Chamption</label><br>
+        <input type="text" id="championselect" name="championselect" class="champbox"></input>
+      </form>
+    </div>
   </body>
 </html>
